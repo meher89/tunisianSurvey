5eed54a7dd6dce9f9898d696f6f0d0a072482ebc
meher89 <meher.ziadi.ensi@gmail.com>
2019-09-14 00:52:24 +0100

    finished login and register

---
+++ src/AppBundle/Form/LoginType.php
@@ -0,0 +1,20 @@
+<?php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', DateType::class)
+            
+        ;
+    }
+}
